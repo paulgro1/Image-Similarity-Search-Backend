65283ea101f89f1a8b3c79278563a1d588b99f20
Joris Müller <s81764@beuth-hochschule.de>
2021-11-24 15:23:43 +0100

    typo

--- server/api_package/similarities.py
+++ server/api_package/similarities.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     exit("Start via run.py!")
 
-def get_similarities2(D):
+def get_similarities(D):
     similarities = 1 / (1 + D / 1e+9)
     return similarities.tolist()
